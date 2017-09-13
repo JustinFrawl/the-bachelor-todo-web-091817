815a1a3bcfb3590d1d143d9aa3bb2a8011ef972e
Justin Frawley <freegamespeech@gmail.com>
2017-09-13 17:45:07 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,54 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestant_hash|
+    if contestant_hash["status"].downcase == "winner"
+      return contestant_hash["name"].split(" ").first
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
+  data.each do |season, contestants|
+    contestants.each do |contestant_hash|
+      if contestant_hash["occupation"] == occupation
+        return contestant_hash["name"]
+      end
+    end
+  end
   # code here
 end
 
 def count_contestants_by_hometown(data, hometown)
+  count = 0
   # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant_hash|
+      if contestant_hash["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
   # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant_hash|
+      if contestant_hash["hometown"] == hometown
+        return contestant_hash["occupation"]
+      end
+    end
+  end
+
 end
 
 def get_average_age_for_season(data, season)
   # code here
+  age_total = 0
+  number_of_contestants = 0
+  data[season].each do |contestant_hash|
+    age_total += (contestant_hash["age"]).to_i
+    number_of_contestants += 1
+  end
+  (age_total/number_of_contestants.to_f).round(0)
 end
